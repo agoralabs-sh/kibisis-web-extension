3826f2d059be47d27a717176b1bb1c3747cef227
Kieran O'Neill <kieran.oneill@pm.me>
2023-03-01 20:12:53 +0200

    feat: add scriot to package extension

---
+++ web-ext-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // global options
+  artifactsDir: 'dist',
+  sourceDir: 'build',
+
+  // build options
+  build: {
+    overwriteDest: true,
+  },
+
+  // run options
+  run: {
+    firefox: '.firefox/firefox',
+  },
+};
