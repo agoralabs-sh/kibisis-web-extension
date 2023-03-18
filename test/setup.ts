3f663062740fc917f0c9ded2afee8f497245bf41
Kieran O'Neill <kieran.oneill@pm.me>
2023-03-18 14:49:46 +0200

    chore: squash

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,11 @@
 import 'mockzilla-webextension';
 
 import { version } from '../package.json';
+import { browser_specific_settings } from '../src/manifest.json';
 
+Object.defineProperty(global, '__AGORA_WALLET_EXTENSION_ID__', {
+  value: browser_specific_settings.gecko.id,
+});
 Object.defineProperty(global, '__ENV__', {
   value: 'test',
 });
