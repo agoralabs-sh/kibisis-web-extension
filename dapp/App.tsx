eddce5b09a5e089f244a4f133fed0756008d0551
Kieran O'Neill <kieran.oneill@pm.me>
2023-03-24 08:21:22 +0200

    chore: update dapp to show table of connected accounts

--- dapp/App.tsx
+++ dapp/App.tsx
@@ -1,13 +1,29 @@
-import { IEnableResult } from '@agoralabs-sh/algorand-provider';
+import {
+  BaseError,
+  IBaseResult,
+  IEnableResult,
+  IWalletAccount,
+} from '@agoralabs-sh/algorand-provider';
 import {
   Center,
   ChakraProvider,
+  CreateToastFnReturn,
   Flex,
   Heading,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
   Text,
+  Th,
+  Thead,
+  Tr,
+  useToast,
   VStack,
 } from '@chakra-ui/react';
-import React, { FC } from 'react';
+import { nanoid } from 'nanoid';
+import React, { FC, useState } from 'react';
 
 // Components
 import Button from '../src/components/Button';
@@ -20,13 +36,40 @@ import { theme } from '../src/theme';
 import { IWindow } from '../src/types';
 
 const App: FC = () => {
+  const toast: CreateToastFnReturn = useToast();
+  const [authorizedAccounts, setAuthorizedAccounts] = useState<
+    IWalletAccount[]
+  >([]);
+  const [genesisId, setGenesisId] = useState<string | null>(null);
   const handleEnableClick = async () => {
     if ((window as IWindow).algorand) {
-      const result: IEnableResult = await (window as IWindow).algorand.enable({
-        genesisHash: 'SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=',
-      });
+      try {
+        const result: IBaseResult & IEnableResult = await (
+          window as IWindow
+        ).algorand.enable({
+          genesisHash: 'SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=',
+        });
 
-      console.log('App#handleEnableClick(): ', result);
+        toast({
+          description: `Successfully connected to "${result.id}".`,
+          duration: 3000,
+          isClosable: true,
+          status: 'success',
+          title: 'Connected!',
+        });
+
+        setAuthorizedAccounts(result.accounts);
+        setGenesisId(result.genesisId);
+      } catch (error: BaseError) {
+        console.log(error);
+        toast({
+          description: error.message,
+          duration: 3000,
+          isClosable: true,
+          status: 'error',
+          title: `${error.code}: ${error.name}`,
+        });
+      }
     }
   };
 
@@ -39,19 +82,42 @@ const App: FC = () => {
           direction="column"
           justifyContent="center"
           minH="100vh"
-          maxW={500}
+          maxW={1024}
           pt={8}
           px={8}
           w="full"
         >
-          <VStack spacing={8} w="full">
-            <VStack justifyContent="center" spacing={3} w="full">
-              <Heading color="gray.500" textAlign="center">
-                Agora Wallet DApp Example
-              </Heading>
-              <Text color="gray.400">Select an operation:</Text>
-            </VStack>
-            <VStack spacing={3} w="full">
+          <VStack justifyContent="center" spacing={8} w="full">
+            <Heading color="gray.500" textAlign="center">
+              Agora Wallet DApp Example
+            </Heading>
+            <TableContainer
+              borderColor="gray.200"
+              borderRadius={15}
+              borderStyle="solid"
+              borderWidth={1}
+              w="full"
+            >
+              <Table variant="simple">
+                <TableCaption>Network: {genesisId || 'N/A'}</TableCaption>
+                <Thead>
+                  <Tr>
+                    <Th>Address</Th>
+                    <Th>Name</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {authorizedAccounts.map((value) => (
+                    <Tr key={nanoid()}>
+                      <Td>{value.address}</Td>
+                      <Td>{value.name || '-'}</Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            </TableContainer>
+            <Text color="gray.400">Select an operation:</Text>
+            <VStack spacing={3} w={500}>
               <Button
                 colorScheme="primary"
                 onClick={handleEnableClick}
