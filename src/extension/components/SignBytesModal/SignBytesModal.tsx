e77a48eecd9bb88f8bf3d4725bac477e686aafae
Kieran O'Neill <kieran.oneill@pm.me>
2023-04-23 08:15:23 +0300

    refactor: move decoded jwt data display to separate component

--- src/extension/components/SignBytesModal/SignBytesModal.tsx
+++ src/extension/components/SignBytesModal/SignBytesModal.tsx
@@ -1,15 +1,9 @@
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
   Avatar,
   Box,
   Code,
   Heading,
   HStack,
-  Icon,
   Modal,
   ModalBody,
   ModalContent,
@@ -18,13 +12,11 @@ import {
   Skeleton,
   SkeletonCircle,
   Text,
-  Tooltip,
   VStack,
 } from '@chakra-ui/react';
 import { nanoid } from 'nanoid';
 import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { IoWarningOutline } from 'react-icons/io5';
 import { useDispatch } from 'react-redux';
 import { randomBytes } from 'tweetnacl';
 
@@ -33,6 +25,7 @@ import AccountSelect from '@extension/components/AccountSelect';
 import AccountItem from '@extension/components/AccountItem';
 import Button from '@extension/components/Button';
 import PasswordInput from '@extension/components/PasswordInput';
+import SignBytesJwtContent from './SignBytesJwtContent';
 
 // Constants
 import { DEFAULT_GAP } from '@extension/constants';
@@ -189,254 +182,14 @@ const SignBytesModal: FC<IProps> = ({ onClose }: IProps) => {
             </>
           )}
         </VStack>
+
         {/* Data display */}
         {decodedJwt ? (
-          <VStack spacing={2} w="full">
-            <Text textAlign="left" w="full">{`${t<string>(
-              'labels.information'
-            )}:`}</Text>
-            {/* Address/sub */}
-            {decodedJwt.payload.subject && (
-              <HStack spacing={2} w="full">
-                <Text fontSize="xs">{`${t<string>('labels.address')}:`}</Text>
-                <Box
-                  backgroundColor={textBackgroundColor}
-                  borderRadius={theme.radii['3xl']}
-                  px={2}
-                  py={1}
-                >
-                  <Text
-                    color={defaultTextColor}
-                    fontSize="xs"
-                    wordBreak="break-word"
-                  >
-                    {decodedJwt.payload.subject}
-                  </Text>
-                </Box>
-                {decodedJwt.payload.subject !== selectedSigner?.address && (
-                  <Tooltip
-                    aria-label="Address does not match the signer"
-                    label={t<string>('captions.addressDoesNotMatch')}
-                  >
-                    <span
-                      style={{
-                        height: '1em',
-                        lineHeight: '1em',
-                      }}
-                    >
-                      <Icon as={IoWarningOutline} color="yellow.500" />
-                    </span>
-                  </Tooltip>
-                )}
-              </HStack>
-            )}
-            {/* Audience/aud */}
-            {decodedJwt.payload.audience && (
-              <HStack spacing={2} w="full">
-                <Text fontSize="xs">{`${t<string>('labels.audience')}:`}</Text>
-                <Box
-                  backgroundColor={textBackgroundColor}
-                  borderRadius={theme.radii['3xl']}
-                  px={2}
-                  py={1}
-                >
-                  <Text
-                    color={defaultTextColor}
-                    fontSize="xs"
-                    wordBreak="break-word"
-                  >
-                    {decodedJwt.payload.audience}
-                  </Text>
-                </Box>
-                {decodedJwt.payload.audience !== signBytesRequest?.host && (
-                  <Tooltip
-                    aria-label="Audience does not match the host"
-                    label={t<string>('captions.audienceDoesNotMatch')}
-                  >
-                    <span
-                      style={{
-                        height: '1em',
-                        lineHeight: '1em',
-                      }}
-                    >
-                      <Icon as={IoWarningOutline} color="yellow.500" />
-                    </span>
-                  </Tooltip>
-                )}
-              </HStack>
-            )}
-            {/* Expiration date/exp */}
-            <HStack spacing={2} w="full">
-              <Text fontSize="xs">{`${t<string>(
-                'labels.expirationDate'
-              )}:`}</Text>
-              <Box
-                backgroundColor={textBackgroundColor}
-                borderRadius={theme.radii['3xl']}
-                px={2}
-                py={1}
-              >
-                <Text
-                  color={defaultTextColor}
-                  fontSize="xs"
-                  wordBreak="break-word"
-                >
-                  {decodedJwt.payload.expiresAt.toLocaleString()}
-                </Text>
-              </Box>
-              {decodedJwt.payload.expiresAt < new Date() && (
-                <Tooltip
-                  aria-label="Expiriation date is in the past"
-                  label={t<string>('captions.securityTokenExpired')}
-                >
-                  <span
-                    style={{
-                      height: '1em',
-                      lineHeight: '1em',
-                    }}
-                  >
-                    <Icon as={IoWarningOutline} color="yellow.500" />
-                  </span>
-                </Tooltip>
-              )}
-            </HStack>
-            {/* More information */}
-            <Accordion allowToggle={true} w="full">
-              <AccordionItem border="none" w="full">
-                <AccordionButton px={0}>
-                  <Text textAlign="left" w="full">{`${t<string>(
-                    'labels.moreInformation'
-                  )}:`}</Text>
-                  <AccordionIcon />
-                </AccordionButton>
-                <AccordionPanel px={0}>
-                  <VStack spacing={2} w="full">
-                    {/* ID/jti */}
-                    {decodedJwt.payload.id && (
-                      <HStack spacing={2} w="full">
-                        <Text fontSize="xs">{`${t<string>(
-                          'labels.id'
-                        )}:`}</Text>
-                        <Box
-                          backgroundColor={textBackgroundColor}
-                          borderRadius={theme.radii['3xl']}
-                          px={2}
-                          py={1}
-                        >
-                          <Text
-                            color={defaultTextColor}
-                            fontSize="xs"
-                            wordBreak="break-word"
-                          >
-                            {decodedJwt.payload.id}
-                          </Text>
-                        </Box>
-                      </HStack>
-                    )}
-                    {/* Issuer/iss */}
-                    {decodedJwt.payload.issuer && (
-                      <HStack spacing={2} w="full">
-                        <Text fontSize="xs">{`${t<string>(
-                          'labels.issuer'
-                        )}:`}</Text>
-                        <Box
-                          backgroundColor={textBackgroundColor}
-                          borderRadius={theme.radii['3xl']}
-                          px={2}
-                          py={1}
-                        >
-                          <Text
-                            color={defaultTextColor}
-                            fontSize="xs"
-                            wordBreak="break-word"
-                          >
-                            {decodedJwt.payload.issuer}
-                          </Text>
-                        </Box>
-                      </HStack>
-                    )}
-                    {/* Issue date/iat */}
-                    {decodedJwt.payload.issuedAt && (
-                      <HStack spacing={2} w="full">
-                        <Text fontSize="xs">{`${t<string>(
-                          'labels.issueDate'
-                        )}:`}</Text>
-                        <Box
-                          backgroundColor={textBackgroundColor}
-                          borderRadius={theme.radii['3xl']}
-                          px={2}
-                          py={1}
-                        >
-                          <Text
-                            color={defaultTextColor}
-                            fontSize="xs"
-                            wordBreak="break-word"
-                          >
-                            {decodedJwt.payload.issuedAt.toLocaleString()}
-                          </Text>
-                        </Box>
-                      </HStack>
-                    )}
-                    {/* Signing method/alg */}
-                    {decodedJwt.header.algorithm && (
-                      <HStack spacing={2} w="full">
-                        <Text fontSize="xs">{`${t<string>(
-                          'labels.signingMethod'
-                        )}:`}</Text>
-                        <Box
-                          backgroundColor={textBackgroundColor}
-                          borderRadius={theme.radii['3xl']}
-                          px={2}
-                          py={1}
-                        >
-                          <Text
-                            color={defaultTextColor}
-                            fontSize="xs"
-                            wordBreak="break-word"
-                          >
-                            {decodedJwt.header.algorithm}
-                          </Text>
-                        </Box>
-                        {decodedJwt.header.curve && (
-                          <Box
-                            borderColor="blue.500"
-                            borderRadius={theme.radii['3xl']}
-                            borderStyle="solid"
-                            borderWidth="1px"
-                            px={2}
-                            py={1}
-                          >
-                            <Text
-                              color="blue.500"
-                              fontSize="xs"
-                              wordBreak="break-word"
-                            >
-                              {decodedJwt.header.curve}
-                            </Text>
-                          </Box>
-                        )}
-                        {decodedJwt.header.algorithm !== 'EdDSA' && (
-                          <Tooltip
-                            aria-label="Invalid signing algorithm"
-                            label={t<string>('captions.invalidAlgorithm')}
-                          >
-                            <span
-                              style={{
-                                height: '1em',
-                                lineHeight: '1em',
-                              }}
-                            >
-                              <Icon as={IoWarningOutline} color="yellow.500" />
-                            </span>
-                          </Tooltip>
-                        )}
-                      </HStack>
-                    )}
-                  </VStack>
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-          </VStack>
+          <SignBytesJwtContent
+            decodedJwt={decodedJwt}
+            host={signBytesRequest.host}
+            signer={selectedSigner}
+          />
         ) : (
           <VStack spacing={2} w="full">
             <Text textAlign="left" w="full">{`${t<string>(
