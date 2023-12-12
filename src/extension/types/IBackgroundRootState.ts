f1046f7033ff1931b8c9806b073b759515eb1089
Kieran O'Neill <kieran.oneill@pm.me>
2023-12-12 12:20:04 +0000

    chore: squash

--- src/extension/types/IBackgroundRootState.ts
+++ src/extension/types/IBackgroundRootState.ts
@@ -1,7 +1,7 @@
 // features
 import { IAccountsState } from '@extension/features/accounts';
 import { IAssetsState } from '@extension/features/assets';
-import { IMessagesState } from '@extension/features/messages';
+import { IEventsState } from '@extension/features/events';
 import { INetworksState } from '@extension/features/networks';
 import { ISessionsState } from '@extension/features/sessions';
 import { ISettingsState } from '@extension/features/settings';
@@ -12,7 +12,7 @@ import IBaseRootState from './IBaseRootState';
 interface IBackgroundRootState extends IBaseRootState {
   accounts: IAccountsState;
   assets: IAssetsState;
-  messages: IMessagesState;
+  events: IEventsState;
   networks: INetworksState;
   sessions: ISessionsState;
   settings: ISettingsState;
