7bc7c25a3ee7dfb19275d60a9891957c86361b5a
Kieran O'Neill <kieran.oneill@pm.me>
2024-01-03 09:56:46 +0000

    chore: squash

--- bin/publish_to_chrome_dummy.sh
+++ bin/publish_to_chrome_dummy.sh
@@ -7,11 +7,11 @@ source "${SCRIPT_DIR}/set_vars.sh"
 function main {
   set_vars
 
-  printf "%b as secrets input: ${1} \n" "${INFO_PREFIX}"
-  printf "%b as env input: ${2} \n" "${INFO_PREFIX}"
-  printf "%b as env var env: ${CHROME_WEB_STORE_ID_AS_ENV} \n" "${INFO_PREFIX}"
-  printf "%b as env var secrets: ${CHROME_WEB_STORE_ID_AS_SECRET} \n" "${INFO_PREFIX}"
-  printf "%b as env from GitHub: ${CHROME_WEB_STORE_ID} \n" "${INFO_PREFIX}"
+  curl \
+      -X POST \
+      -s \
+      "https://www.googleapis.com/oauth2/v4/token" \
+      -d "client_id=${CHROME_WEB_STORE_API_CLIENT_ID}&client_secret=${CHROME_WEB_STORE_API_CLIENT_SECRET}&refresh_token=${CHROME_WEB_STORE_API_REFRESH_TOKEN}&grant_type=refresh_token"
 
   exit 0
 }
