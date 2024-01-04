7b7ab39cb2d96efa361b6706fa887b16caf52fe7
Kieran O'Neill <kieran.oneill@pm.me>
2024-01-04 13:29:22 +0000

    feat: use external link for strong password policy

--- src/extension/components/CreatePasswordInput/CreatePasswordInput.tsx
+++ src/extension/components/CreatePasswordInput/CreatePasswordInput.tsx
@@ -4,9 +4,9 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  Link,
   Text,
   VStack,
-  useDisclosure,
 } from '@chakra-ui/react';
 import zxcvbn from 'zxcvbn';
 import React, {
@@ -17,20 +17,20 @@ import React, {
   useState,
 } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { IoEye, IoEyeOff } from 'react-icons/io5';
+import { IoEye, IoEyeOff, IoOpenOutline } from 'react-icons/io5';
 
 // components
 import IconButton from '@extension/components/IconButton';
 import StrengthMeter from '@extension/components/StrengthMeter';
+
+// constants
+import { STRONG_PASSWORD_POLICY_LINK } from '@extension/constants';
 
 // hooks
 import useDefaultTextColor from '@extension/hooks/useDefaultTextColor';
 import usePrimaryColor from '@extension/hooks/usePrimaryColor';
 import usePrimaryColorScheme from '@extension/hooks/usePrimaryColorScheme';
 import useSubTextColor from '@extension/hooks/useSubTextColor';
-
-// modals
-import DocumentModal from '@extension/modals//DocumentModal';
 
 // utils
 import { validate } from './utils';
@@ -55,7 +55,6 @@ const CreatePasswordInput: FC<IProps> = ({
   value,
 }: IProps) => {
   const { t } = useTranslation();
-  const { isOpen, onClose, onOpen } = useDisclosure();
   // hooks
   const defaultTextColor: string = useDefaultTextColor();
   const primaryColor: string = usePrimaryColor();
@@ -78,82 +77,76 @@ const CreatePasswordInput: FC<IProps> = ({
 
     onChange(value, newScore);
   };
-  const handleOpenStrongPasswordPolicy = () => onOpen();
   const handleShowHideClick = () => setShow(!show);
 
   return (
-    <>
-      <DocumentModal
-        documentName="strong_password_policy"
-        isOpen={isOpen}
-        onClose={onClose}
-        title={t<string>('titles.strongPasswordPolicy')}
-      />
-      <VStack>
-        <HStack alignItems="flex-end" justifyContent="space-between" w="full">
-          {/*label*/}
-          <Text
-            as={'label'}
-            color={error ? 'red.300' : defaultTextColor}
-            htmlFor={inputId}
-            textAlign="left"
-          >
-            {label || t<string>('labels.password')}
-          </Text>
-
-          <Text color="red.300" fontSize="xs" textAlign="right">
-            {error}
-          </Text>
-        </HStack>
-
-        {/*input*/}
-        <InputGroup size="md">
-          <Input
-            autoComplete="new-password"
-            disabled={disabled}
-            focusBorderColor={error ? 'red.300' : primaryColor}
-            id={inputId}
-            isInvalid={!!error}
-            onChange={handleOnChange}
-            onKeyUp={onKeyUp}
-            placeholder={t<string>('placeholders.enterPassword')}
-            ref={inputRef}
-            type={show ? 'text' : 'password'}
-            value={value}
-          />
-
-          <InputRightElement>
-            <IconButton
-              aria-label="Eye open and closed"
-              disabled={disabled}
-              icon={show ? IoEye : IoEyeOff}
-              onClick={handleShowHideClick}
-              size="sm"
-              variant="ghost"
-            />
-          </InputRightElement>
-        </InputGroup>
-
-        {/*strong password policy*/}
-        <Text color={subTextColor} fontSize="xs" textAlign="left">
-          <Trans i18nKey="captions.passwordScoreInfo">
-            To conform with our{' '}
-            <Button
-              colorScheme={primaryColorScheme}
-              fontSize="xs"
-              onClick={handleOpenStrongPasswordPolicy}
-              variant="link"
-            >
-              Strong Password Policy
-            </Button>
-            , you are required to use a sufficiently strong password. Password
-            must be at least 8 characters.
-          </Trans>
+    <VStack w="full">
+      <HStack alignItems="flex-end" justifyContent="space-between" w="full">
+        {/*label*/}
+        <Text
+          as={'label'}
+          color={error ? 'red.300' : defaultTextColor}
+          htmlFor={inputId}
+          textAlign="left"
+        >
+          {label || t<string>('labels.password')}
         </Text>
 
-        <StrengthMeter score={score} />
-      </VStack>
-    </>
+        <Text color="red.300" fontSize="xs" textAlign="right">
+          {error}
+        </Text>
+      </HStack>
+
+      {/*input*/}
+      <InputGroup size="md">
+        <Input
+          autoComplete="new-password"
+          disabled={disabled}
+          focusBorderColor={error ? 'red.300' : primaryColor}
+          id={inputId}
+          isInvalid={!!error}
+          onChange={handleOnChange}
+          onKeyUp={onKeyUp}
+          placeholder={t<string>('placeholders.enterPassword')}
+          ref={inputRef}
+          type={show ? 'text' : 'password'}
+          value={value}
+        />
+
+        <InputRightElement>
+          <IconButton
+            aria-label="Eye open and closed"
+            disabled={disabled}
+            icon={show ? IoEye : IoEyeOff}
+            onClick={handleShowHideClick}
+            size="sm"
+            variant="ghost"
+          />
+        </InputRightElement>
+      </InputGroup>
+
+      {/*strong password policy*/}
+      <Text color={subTextColor} fontSize="xs" textAlign="left">
+        <Trans i18nKey="captions.passwordScoreInfo">
+          To conform with our{' '}
+          <Button
+            as={Link}
+            colorScheme={primaryColorScheme}
+            fontSize="xs"
+            href={STRONG_PASSWORD_POLICY_LINK}
+            rightIcon={<IoOpenOutline />}
+            target="_blank"
+            variant="link"
+          >
+            Strong Password Policy
+          </Button>
+          , you are required to use a sufficiently strong password. Password
+          must be at least 8 characters.
+        </Trans>
+      </Text>
+
+      <StrengthMeter score={score} />
+    </VStack>
   );
 };
 
--- src/extension/constants/Links.ts
+++ src/extension/constants/Links.ts
@@ -1,3 +1,5 @@
 export const AGORA_LABS_LINK: string = 'https://agoralabs.sh';
-export const KIBISIS_LINK: string = 'https://kibisi.is';
+export const KIBISIS_LINK: string = 'https://kibis.is';
+export const STRONG_PASSWORD_POLICY_LINK: string =
+  'https://kibis.is/strong-password-policy';
 export const SUPPORT_MAIL_TO_LINK: string = 'mailto:support@agoralabs.sh';
