e57a0b4110b8bd1ceba12f69ae33ab082bc9db76
Kieran O'Neill <kieran.oneill@pm.me>
2024-01-04 13:30:36 +0000

    refactor: remove react-markdown package and remove document modeal

--- src/extension/modals/DocumentModal/hooks/useDocument/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './useDocument';
-export * from './types';
--- src/extension/modals/DocumentModal/hooks/useDocument/types/index.ts
+++
@@ -1,2 +0,0 @@
-export type { default as IDocument } from './IDocument';
-export type { default as IUseDocumentState } from './IUseDocumentState';
