9afd9fb87001d2e9e334e4d29d586da73524e91b
Kieran O'Neill <kieran.oneill@pm.me>
2024-01-08 15:18:36 +0000

    chore: squash

--- src/external/constants/Arc0013.ts
+++
@@ -1,2 +0,0 @@
-export const ARC_0013_PREFIX: string = 'arc0013';
-export const ARC_0013_CHANNEL_NAME: string = `${ARC_0013_PREFIX}:channel`;
--- src/external/constants/index.ts
+++ src/external/constants/index.ts
@@ -1,3 +1,2 @@
 export * from './Application';
-export * from './Arc0013';
 export * from './Durations';
