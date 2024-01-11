12bf9ee994d998cc8b4c464e281e86c116b8aec2
Kieran O'Neill <kieran.oneill@pm.me>
2024-01-11 10:49:02 +0000

    chore: squash

--- src/extension/modals/SignTxnsModal/SignTxnsModal.tsx
+++ src/extension/modals/SignTxnsModal/SignTxnsModal.tsx
@@ -198,6 +198,7 @@ const SignTxnsModal: FC<IProps> = ({ onClose }: IProps) => {
               stxns: null,
             })
           );
+          break;
         default:
           dispatch(
             createNotification({
