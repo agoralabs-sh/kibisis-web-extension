45deaf785473add929121af7ff6ead754277394a
Kieran O'Neill <kieran.oneill@pm.me>
2024-01-17 16:01:16 +0000

    fix: mainnet checked state correctly passed to settings update

--- src/extension/features/settings/thunks/saveSettingsToStorage.ts
+++ src/extension/features/settings/thunks/saveSettingsToStorage.ts
@@ -33,6 +33,7 @@ const saveSettingsToStorage: AsyncThunk<
     });
     let selectedNetwork: INetworkWithTransactionParams | null =
       selectNetworkFromSettings(networks, settings);
+    console.log(settings.advanced);
 
     // if the beta/main-net has been disallowed and the selected network is one of the disallowed, set it to a test one
     if (
