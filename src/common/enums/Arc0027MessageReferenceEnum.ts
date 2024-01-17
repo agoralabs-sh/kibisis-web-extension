8450512708969ebbdb1bea17315dabfa427b42fd
Kieran O'Neill <hello@kieranoneill.com>
2024-01-17 10:52:29 +0000

    fix: replace arc0013 references with correct arc0027 (#130)

--- src/common/enums/Arc0027MessageReferenceEnum.ts
+++ src/common/enums/Arc0027MessageReferenceEnum.ts
@@ -1,12 +1,12 @@
 enum Arc0027MessageReferenceEnum {
-  EnableRequest = `arc0013:enable:request`,
-  EnableResponse = `arc0013:enable:response`,
-  GetProvidersRequest = `arc0013:get_providers:request`,
-  GetProvidersResponse = `arc0013:get_providers:response`,
-  SignBytesRequest = `arc0013:sign_bytes:request`,
-  SignBytesResponse = `arc0013:sign_bytes:response`,
-  SignTxnsRequest = `arc0013:sign_txns:request`,
-  SignTxnsResponse = `arc0013:sign_txns:response`,
+  EnableRequest = `arc0027:enable:request`,
+  EnableResponse = `arc0027:enable:response`,
+  GetProvidersRequest = `arc0027:get_providers:request`,
+  GetProvidersResponse = `arc0027:get_providers:response`,
+  SignBytesRequest = `arc0027:sign_bytes:request`,
+  SignBytesResponse = `arc0027:sign_bytes:response`,
+  SignTxnsRequest = `arc0027:sign_txns:request`,
+  SignTxnsResponse = `arc0027:sign_txns:response`,
 }
 
 export default Arc0027MessageReferenceEnum;
