6a05e6f82d727efc253a40aa706f4f60508cd11f
Kieran O'Neill <kieran.oneill@pm.me>
2024-02-04 09:28:15 +0000

    build(dapp-example): rename dapp directory to dapp-example

--- webpack/constants/Directories.ts
+++ webpack/constants/Directories.ts
@@ -4,8 +4,14 @@ export const CHROME_BUILD_PATH: string = resolve(
   process.cwd(),
   '.chrome_build'
 );
-export const DAPP_BUILD_PATH: string = resolve(process.cwd(), '.dapp');
-export const DAPP_SRC_PATH: string = resolve(process.cwd(), 'dapp');
+export const DAPP_EXAMPLE_BUILD_PATH: string = resolve(
+  process.cwd(),
+  '.dapp-example_build'
+);
+export const DAPP_EXAMPLE_SRC_PATH: string = resolve(
+  process.cwd(),
+  'dapp-example'
+);
 export const EDGE_BUILD_PATH: string = resolve(process.cwd(), '.edge_build');
 export const FIREFOX_BUILD_PATH: string = resolve(
   process.cwd(),
