ff0d9b42d347e62fedc18e185dbe61c52f0986fd
Kieran O'Neill <kieran.oneill@pm.me>
2024-02-21 22:45:46 +0200

    feat: add uri parsing for adding assets

--- src/extension/types/IARC0300AccountImportSchema.ts
+++ src/extension/types/IARC0300AccountImportSchema.ts
@@ -1,5 +1,6 @@
 // enums
 import {
+  ARC0300AuthorityEnum,
   ARC0300EncodingEnum,
   ARC0300PathEnum,
   ARC0300QueryEnum,
@@ -15,6 +16,7 @@ interface IARC0300AccountImportQuery {
 }
 
 interface IARC0300AccountImportSchema extends IARC0300BaseSchema {
+  authority: ARC0300AuthorityEnum.Account;
   paths: [ARC0300PathEnum.Import];
   query: IARC0300AccountImportQuery;
 }
