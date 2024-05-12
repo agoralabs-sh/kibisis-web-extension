47291f0f114d7a5a9f3538507495e38d462d9623
Kieran O'Neill <hello@kieranoneill.com>
2024-05-12 17:27:14 +0100

    refactor: remove unused wallet connect selectors

--- src/extension/selectors/useSelectInitializingWalletConnect.ts
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-
-// types
-import { IMainRootState } from '@extension/types';
-export default function useSelectInitializingWalletConnect(): boolean {
-  return useSelector<IMainRootState, boolean>(
-    (state) => state.sessions.initializingWalletConnect
-  );
-}
