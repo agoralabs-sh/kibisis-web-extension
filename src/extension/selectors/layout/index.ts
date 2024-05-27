f140da172e0c25bf76cb4f42793375a49849e334
Kieran O'Neill <hello@kieranoneill.com>
2024-05-27 20:41:53 +0100

    chore: squash

--- src/extension/selectors/layout/index.ts
+++ src/extension/selectors/layout/index.ts
@@ -1,4 +1,3 @@
 export { default as useSelectConfirmModal } from './useSelectConfirmModal';
-export { default as useSelectDeviceID } from './useSelectDeviceID';
 export { default as useSelectScanQRCodeModal } from './useSelectScanQRCodeModal';
 export { default as useSelectSideBar } from './useSelectSideBar';
