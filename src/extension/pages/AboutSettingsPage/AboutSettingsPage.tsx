d332d64650fc9bfc42700a769c8938aab496069f
Kieran O'Neill <hello@kieranoneill.com>
2024-08-11 15:14:42 +0100

    feat: add device id to about page

--- src/extension/pages/AboutSettingsPage/AboutSettingsPage.tsx
+++ src/extension/pages/AboutSettingsPage/AboutSettingsPage.tsx
@@ -1,17 +1,23 @@
-import { Text, VStack } from '@chakra-ui/react';
+import { HStack, Text, VStack } from '@chakra-ui/react';
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import browser from 'webextension-polyfill';
 
 // components
+import CopyIconButton from '@extension/components/CopyIconButton';
 import PageHeader from '@extension/components/PageHeader';
 import SettingsTextItem from '@extension/components/SettingsTextItem';
 
 // hooks
 import useSubTextColor from '@extension/hooks/useSubTextColor';
 
+// selectors
+import { useSelectSystemInfo } from '@extension/selectors';
+
 const AboutSettingsPage: FC = () => {
   const { t } = useTranslation();
+  // selectors
+  const systemInfo = useSelectSystemInfo();
   // hooks
   const subTextColor: string = useSubTextColor();
 
@@ -23,10 +29,45 @@ const AboutSettingsPage: FC = () => {
       <VStack w="full">
         {/*extension id*/}
         <SettingsTextItem fontSize="sm" label={t<string>('labels.extensionId')}>
-          <Text color={subTextColor} size="sm" textAlign="right">
-            {browser.runtime.id}
-          </Text>
+          <HStack
+            alignItems="center"
+            justifyContent="flex-end"
+            spacing={1}
+            w="full"
+          >
+            <Text color={subTextColor} size="sm" textAlign="right">
+              {browser.runtime.id}
+            </Text>
+
+            <CopyIconButton
+              ariaLabel={t<string>('labels.copyExtensionID')}
+              tooltipLabel={t<string>('labels.copyExtensionID')}
+              value={browser.runtime.id}
+            />
+          </HStack>
         </SettingsTextItem>
+
+        {/*device id*/}
+        {systemInfo?.deviceID && (
+          <SettingsTextItem fontSize="sm" label={t<string>('labels.deviceID')}>
+            <HStack
+              alignItems="center"
+              justifyContent="flex-end"
+              spacing={1}
+              w="full"
+            >
+              <Text color={subTextColor} size="sm" textAlign="right">
+                {systemInfo.deviceID}
+              </Text>
+
+              <CopyIconButton
+                ariaLabel={t<string>('labels.copyDeviceID')}
+                tooltipLabel={t<string>('labels.copyDeviceID')}
+                value={systemInfo.deviceID}
+              />
+            </HStack>
+          </SettingsTextItem>
+        )}
 
         {/*version*/}
         <SettingsTextItem fontSize="sm" label={t<string>('labels.version')}>
