309111da83e92e0f2c4519d9ef4b365675aed39b
Kieran O'Neill <hello@kieranoneill.com>
2024-08-25 10:53:32 +0100

    chore: squash

--- webpack/plugins/WebExtPlugin/WebExtPlugin.ts
+++ webpack/plugins/WebExtPlugin/WebExtPlugin.ts
@@ -83,8 +83,6 @@ export default class WebExtPlugin {
       ];
     }
 
-    console.log(runCommand.join(' '));
-
     // start web-ext
     this.webExtRunProcess = spawn('web-ext', runCommand);
   }
