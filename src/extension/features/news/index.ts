9c9587ba1fc38a46333b00faca4fd563935fca4f
Kieran O'Neill <hello@kieranoneill.com>
2024-09-16 15:30:58 +0100

    refactor: remove voiage to mainnet message and news item

--- src/extension/features/news/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './slice';
-export * from './thunks';
-export * from './types';
-export * from './utils';
--- src/extension/selectors/news/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as useSelectNewsFetching } from './useSelectNewsFetching';
-export { default as useSelectNewsItemByName } from './useSelectNewsItemByName';
-export { default as useSelectNewsSaving } from './useSelectNewsSaving';
